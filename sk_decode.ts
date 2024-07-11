2a13a59608a2ffe9f9615fce0fa331b29bf5463e
betullisler <betulisler16@gmail.com>
2024-07-11 14:13:54 +0300

    first commit

---
+++ sk_decode.ts
@@ -0,0 +1,5 @@
+import bs58 from "bs58";
+
+const kp = bs58.decode("5EKoaCC7VrDbjtcE2vzRRracPvPGUSmxKEPa8pdbY3r7YhFFroonmNrFGBjHc9aneGY6ytXQcfxYhySD4eY5YXM4");
+
+console.log("my secret key is: " + kp);
